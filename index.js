aa7706839375b9e78df650f19cac3c2adaf92f4e
katie-soft <analyst.meo@gmail.com>
2024-02-26 19:21:54 +0300

    feat: add preview button

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const adminDomain = 'secrets-wp.tcsgroup.io';
 const prodDomain = 'secrets.tinkoff.ru';
+const previewDomain = 'https://secrets.tinkoff.ru/preview/';
 
-const button = document.querySelector('#button');
+const editButton = document.querySelector('#button-edit');
+const previewButton = document.querySelector('#button-preview');
 
-const replaceUrl = () => {
+const openUrl = () => {
   chrome.tabs.query({active: true, currentWindow: true}, tabs => {
     const currentUrl = tabs[0].url;
     if (currentUrl.includes(prodDomain)) {
       const adminUrl = currentUrl.replace(prodDomain, adminDomain);
-      window.open(adminUrl, '_blank')
+      window.open(adminUrl, '_blank');
+   } else if (currentUrl.includes(adminDomain)) {
+      const id = currentUrl.slice(54, 59);
+      window.open(`${previewDomain}${id}`, '_blank')
    } else {
       alert('Расширение работает только на сайте БС');
    }
 });
 }
 
-button.addEventListener('click', replaceUrl);
+editButton.addEventListener('click', openUrl);
+previewButton.addEventListener('click', openUrl);
